4b102b23cbb25d24a8f8757a8d90f8aca000ecda
Nariman <nariman.ospanov@ensek.co.uk>
2026-03-08 21:50:18 +0500

    one lineempty  change to trigger ci cd

--- src/db.js
+++ src/db.js
@@ -2,6 +2,7 @@ import { Sequelize } from 'sequelize';
 import { config } from './config.js';
 import { initModels } from './models/index.js';
 
+
 const sequelize = new Sequelize(config.db.database, config.db.username, config.db.password, {
   host: config.db.host,
   port: config.db.port,
